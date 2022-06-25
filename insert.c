4059cd960ad80749df8800fb718b070135eb8910
Pierre <pierre.forstmann@free.fr>
2022-06-25 16:45:20 +0200

    insert: added go for rollback

--- insert.c
+++ insert.c
@@ -87,6 +87,7 @@ int main(int argc, char **argv) {
     * insert question
     */ 
 
+restart:
     begin_transaction(db);
 
     printf("Enter question:");
@@ -102,28 +103,39 @@ int main(int argc, char **argv) {
     rc = sqlite3_prepare_v2(db, "INSERT INTO q(question, solution) VALUES(?1, ?2)", -1, &res, &tail);
     if (rc != SQLITE_OK) {
 	rollback_transaction("Failed to prepare INSERT INTO q", db);
+	goto restart;
 	}
 	
     rc = sqlite3_bind_text(res, 1, col1, -1, SQLITE_STATIC); 
     if (rc != SQLITE_OK) {
         rollback_transaction("Failed to bind INSERT param. 1", db);
+	goto restart;
 	}
 
     rc = sqlite3_bind_int(res, 2, col2); 
     if (rc != SQLITE_OK) {
         rollback_transaction("Failed to bind INSERT param. 2", db);
+	goto restart;
 	}
 
     rc = sqlite3_step(res);
     if (rc != SQLITE_DONE) {
          rollback_transaction("Failed to INSERT INTO q", db);
+	goto restart;
     }
     last_id = sqlite3_last_insert_rowid(db);
     printf("The last question id. is: %d\n", last_id);
 
+    rc = sqlite3_finalize(res);
+    if (rc != SQLITE_OK) {
+         rollback_transaction("Failed to delete prepared statement INSERT INTO q", db);
+	goto restart;
+	}
+
     /*
      * insert answers
      */
+
     col3 = (char *)malloc(ANSWER_MAX_LENGTH);
     for (i = 1 ; i <= MAX_ANSWER_NB; i++)
     {
@@ -131,16 +143,19 @@ int main(int argc, char **argv) {
 			             -1, &res, &tail);
         if (rc != SQLITE_OK) {
           rollback_transaction("Failed to prepare INSERT INTO a", db);
+	  goto restart;
 	}
 
         rc = sqlite3_bind_int(res, 1, last_id); 
         if (rc != SQLITE_OK) {
           rollback_transaction("Failed to bind INSERT param. 1", db);
+	  goto restart;
 	}
 
         rc = sqlite3_bind_int(res, 2, i); 
         if (rc != SQLITE_OK) {
          rollback_transaction("Failed to bind INSERT param. 2", db);
+	 goto restart;
 	}
 
     	printf("Enter answser:");
@@ -148,13 +163,21 @@ int main(int argc, char **argv) {
         rc = sqlite3_bind_text(res, 3, col3, -1, SQLITE_STATIC); 
         if (rc != SQLITE_OK) {
          rollback_transaction("Failed to bind INSERT param. 3", db);
+	 goto restart;
 	}
 
         rc = sqlite3_step(res);
         if (rc != SQLITE_DONE) {
          rollback_transaction("Failed to INSERT INTO a", db);
+	  goto restart;
         }
     }
+    rc  = sqlite3_finalize(res);
+    if (rc != SQLITE_OK) {
+         rollback_transaction("Failed to delete prepared statement INSERT INTO a", db);
+	goto restart;
+	}
+
 
     rc = sqlite3_exec(db, "COMMIT TRANSACTION", NULL, NULL, NULL);
     if (rc != SQLITE_OK) {
@@ -169,7 +192,6 @@ int main(int argc, char **argv) {
      * exit
      */
 
-    sqlite3_finalize(res);
     sqlite3_close(db);
     
     return 0;
